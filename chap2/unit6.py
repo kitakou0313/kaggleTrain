9faf52b326079d62806e8cc17203e5ca7e6d409f
kitakou0313 <kitakou0313@gmail.com>
2021-08-17 14:57:15 +0000

    Import modules

---
+++ chap2/unit6.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from torchinfo import summary
+import numpy as np
+import os
+
+from pytorchcv.pytorchcv import train, plot_results, display_dataset, train_long, check_image_dir
+
