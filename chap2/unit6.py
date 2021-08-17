519e094b55a8e11c8862cec0a9fae4c613bb6523
kitakou0313 <kitakou0313@gmail.com>
2021-08-17 15:21:37 +0000

    Add date init proc

--- chap2/unit6.py
+++ chap2/unit6.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision
+from torchvision import datasets
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from torchinfo import summary
@@ -9,3 +10,23 @@ import os
 
 from pytorchcv.pytorchcv import train, plot_results, display_dataset, train_long, check_image_dir
 
+import zipfile
+if not os.path.exists('data/PetImages'):
+    with zipfile.ZipFile('data/kagglecatsanddogs_3367a.zip', 'r') as zip_ref:
+        zip_ref.extractall('data')
+
+check_image_dir('data/PetImages/Cat/*.jpg')
+check_image_dir('data/PetImages/Dog/*.jpg')
+
+std_normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225])
+
+trans = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    std_normalize
+])
+
+dataset = torchvision.datasets.ImageFolder("data/PetImages", transform=trans)
+trainset, testset = torch.utils.data.random_split(dataset, [20000, len(dataset) - 20000])
