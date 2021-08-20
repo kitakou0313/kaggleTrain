fab4c0c048ed2e0204df97bdaba0fda6a2f5fb55
kitakou0313 <kitakou0313@gmail.com>
2021-08-20 15:04:52 +0000

    Add sample code for unit7

--- chap2/unit7.py
+++ chap2/unit7.py
@@ -6,3 +6,9 @@ from torchinfo import summary
 import os
 
 from pytorchcv.pytorchcv import train, display_dataset, train_long, load_cats_dogs_dataset, validate, common_transform
+
+dataset, train_loader, test_loader = load_cats_dogs_dataset()
+
+model = torch.hub.load('pytorch/vision:v0.6.0', 'mobilenet_v2', pretrained=True)
+model.eval()
+print(model)
