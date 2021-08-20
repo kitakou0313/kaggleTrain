254687e8fe27ef8e3a9c5c7325f56cf46f2866a8
kitakou0313 <kitakou0313@gmail.com>
2021-08-20 15:11:00 +0000

    Change model loading to not trained

--- chap2/unit7.py
+++ chap2/unit7.py
@@ -9,6 +9,6 @@ from pytorchcv.pytorchcv import train, display_dataset, train_long, load_cats_do
 
 dataset, train_loader, test_loader = load_cats_dogs_dataset()
 
-model = torch.hub.load('pytorch/vision:v0.6.0', 'mobilenet_v2', pretrained=True)
+model = torch.hub.load('pytorch/vision:v0.6.0', 'mobilenet_v2', pretrained=False)
 model.eval()
 print(model)
