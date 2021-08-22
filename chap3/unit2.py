cd2a3787139e217f6959ac2ed1c5e2a0cfdbdedb
kitakou0313 <kitakou0313@gmail.com>
2021-08-22 15:19:15 +0000

    Add code for tokenizer

--- chap3/unit2.py
+++ chap3/unit2.py
@@ -6,4 +6,21 @@ os.makedirs('./data',exist_ok=True)
 train_dataset, test_dataset = torchtext.datasets.AG_NEWS(root='./data', split=('train', 'test'))
 classes = ['World', 'Sports', 'Business', 'Sci/Tech']
 
-print(next(train_dataset))
+train_dataset = list(train_dataset)
+test_dataset = list(test_dataset)
+
+tokenizer = torchtext.data.utils.get_tokenizer("basic_english")
+
+counter = collections.Counter()
+for (lavel, line) in train_dataset:
+    counter.update(tokenizer(line))
+
+vocab = torchtext.vocab.vocab(counter, min_freq=1)
+
+vocab_size = len(vocab)
+print(f"Vocab size if {vocab_size}")
+
+def encode(x):
+    return [vocab.get_stoi()[s] for s in tokenizer(x)]
+
+print(encode('I love to play with my words'))
