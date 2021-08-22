c3ad7cb04848c2756c1250a8737d1d845ec7761f
kitakou0313 <kitakou0313@gmail.com>
2021-08-22 15:03:47 +0000

    Load date

--- chap3/unit2.py
+++ chap3/unit2.py
@@ -1,7 +1,11 @@
+from typing import NewType
 import torch
+from torch._C import Node
 import torchtext
 import os
 import collections
 os.makedirs('./data',exist_ok=True)
-train_dataset, test_dataset = torchtext.datasets.AG_NEWS(root='./data')
+train_dataset, test_dataset = torchtext.datasets.AG_NEWS(root='./data', split=('train', 'test'))
 classes = ['World', 'Sports', 'Business', 'Sci/Tech']
+
+print(next(train_dataset))
