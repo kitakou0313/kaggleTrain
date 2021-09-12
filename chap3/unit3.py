e736f685d9870f9942bf781cb997ad07382ec48c
kitakou0313 <kitakou0313@gmail.com>
2021-09-12 12:02:34 +0000

    Romove duplicate def

--- chap3/unit3.py
+++ chap3/unit3.py
@@ -7,31 +7,28 @@ train_dataset, test_dataset, classes, vocab = load_dataset()
 vocab_size = len(vocab)
 print("Vocab size = ",vocab_size)
 
-class EmbedClassifier(torch.nn.Module):
-    """
-    embeddig層を追加
-    """
-    def __init__(self, vocab_size:int, embed_dim:int, num_class:int):
-        """
-        docstring
-        """
-        super().__init__()
-        self.embedding = torch.nn.Embedding(vocab_size, embed_dim)
-        self.fc = torch.nn.Linear(embed_dim, num_class)
+# class EmbedClassifier(torch.nn.Module):
+#     """
+#     embeddig層を追加
+#     """
+#     def __init__(self, vocab_size:int, embed_dim:int, num_class:int):
+#         """
+#         docstring
+#         """
+#         super().__init__()
+#         self.embedding = torch.nn.Embedding(vocab_size, embed_dim)
+#         self.fc = torch.nn.Linear(embed_dim, num_class)
 
-    def forward(self, x):
-        """
-        docstring
-        """
-        x = self.embedding(x)
-        x = torch.mean(x, dim=1)
+#     def forward(self, x):
+#         """
+#         docstring
+#         """
+#         x = self.embedding(x)
+#         x = torch.mean(x, dim=1)
 
-        return self.fc(x)
+#         return self.fc(x)
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, collate_fn=padify, shuffle=True)
-
-net = EmbedClassifier(vocab_size, 32, len(classes)).to(device)
-train_epoch(net, train_loader, lr=1, epoch_size=25000)
 
 class EmbedClassifier(torch.nn.Module):
     """
