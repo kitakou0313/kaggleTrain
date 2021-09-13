eabecac8053c5a1ce695c97810cc63e442157537
kitakou0313 <kitakou0313@gmail.com>
2021-09-13 14:26:27 +0000

    Add func to load dataset

--- chap4/unit2.py
+++ chap4/unit2.py
@@ -1,7 +1,22 @@
 import os
 import torch
+from torch.utils import data
 import torchaudio
 
 from torch.utils.data import DataLoader, Dataset
 from pathlib import Path
 
+def load_audio_files(path:str, label:str):
+    dataset = []
+    walker = sorted(str(p) for p in Path(path).glob(f'*.wav'))
+
+    for i, file_path in enumerate(walker):
+        path, filename = os,path.split(file_path)
+        speaker, _ = os.path.splitext(filename)
+        speaker_id, utterance_number = speaker.split("_nohash_")
+        utterance_number = int(utterance_number)
+
+        waveform, sample_rate = torchaudio.load(file_path)
+        dataset.append([waveform, sample_rate, label, speaker_id, utterance_number])
+    
+    return dataset
